d767b440910a7fe1100db4558e83de2b52cd7905
mhaddaoui <mhaddaou@student.1337.ma>
2022-10-30 05:41:32 -0400

    ex02 not yet

---
+++ ex02/Array.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaddaou < mhaddaou@student.1337.ma>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 05:34:12 by mhaddaou          #+#    #+#             */
+/*   Updated: 2022/10/30 05:37:56 by mhaddaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <string>
+
+template <class T>
+class Array{
+    public:
+        Array(){
+            
+            
+        };
+};
+
+#endif
